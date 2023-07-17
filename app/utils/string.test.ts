852a6ebcf0808fca13588700871264afce4b1509
Joël Digbeu <87069318+JoelDigbeu@users.noreply.github.com>
2023-07-17 13:34:31 +0100

    add isString util

--- app/utils/string.test.ts
+++ app/utils/string.test.ts
@@ -1,13 +1,25 @@
-import { validateEmail } from './string'
+import { validateEmail, isString } from './string'
 
-test('validateEmail returns false for non-emails', () => {
-  expect(validateEmail(undefined)).toBe(false)
-  expect(validateEmail(null)).toBe(false)
-  expect(validateEmail('')).toBe(false)
-  expect(validateEmail('not-an-email')).toBe(false)
-  expect(validateEmail('n@')).toBe(false)
+describe('validateEmail', () => {
+  it('returns false for non-emails', () => {
+    expect(validateEmail(undefined)).toBe(false)
+    expect(validateEmail(null)).toBe(false)
+    expect(validateEmail('')).toBe(false)
+    expect(validateEmail('not-an-email')).toBe(false)
+    expect(validateEmail('n@')).toBe(false)
+  })
+
+  it('returns true for emails', () => {
+    expect(validateEmail('kody@example.com')).toBe(true)
+  })
 })
 
-test('validateEmail returns true for emails', () => {
-  expect(validateEmail('kody@example.com')).toBe(true)
+describe('isString', () => {
+  it('returns true for string', () => {
+    expect(isString('kody@example.com')).toBe(true)
+  })
+
+  it('returns false for other types', () => {
+    expect(isString(5)).toBe(false)
+  })
 })
