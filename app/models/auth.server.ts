68ccc857bf14c455eda0cf916d4719fa1125d41f
Joël Digbeu <87069318+JoelDigbeu@users.noreply.github.com>
2023-07-17 15:48:25 +0100

    add auth server

---
+++ app/models/auth.server.ts
@@ -0,0 +1,21 @@
+import bcrypt from 'bcryptjs'
+
+import { prisma } from '~/db.server'
+
+type LoginForm = {
+  password: string
+  username: string
+}
+
+export async function login({ password, username }: LoginForm) {
+  const user = await prisma.user.findUnique({
+    where: { username },
+  })
+
+  if (!user) return null
+
+  const isCorrectPassword = await bcrypt.compare(password, user.passwordHash)
+  if (!isCorrectPassword) return null
+
+  return { id: user.id, username }
+}
